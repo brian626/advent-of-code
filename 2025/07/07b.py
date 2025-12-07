fe0c221aba2ee555d6afd727669b5d45be90a4f9
Brian Eck <brianeck@gmail.com>
2025-12-07 17:21:32 -0500

    Works but too slow

--- 2025/07/07b.py
+++ 2025/07/07b.py
@@ -1,9 +1,9 @@
 
-from typing import List, Tuple
+from typing import List, Tuple, Set
 
 lines = []
 
-with open('07.test', 'r') as f:
+with open('07.input', 'r') as f:
     lines = f.readlines()
 
 lines = [line.strip() for line in lines]
@@ -12,82 +12,49 @@ grid = [list(line) for line in lines]
 grid_width = len(grid[0])
 grid_height = len(grid)
 
-beams: List[Tuple[int, int]] = []
+paths: List[List[Tuple[int, int]]] = []
 
 # Find the initial position
 for col in range(grid_width):
     if grid[0][col] == 'S':
-        beams.append((0, col))
+        paths.append([(0, col)])
         break
 
-num_splits = 0
+solutions: Set[str] = set()
 
-def print_grid():
-    for row in range(len(grid)):
-        print(''.join(grid[row]))
-    print()
+def print_paths(paths):
+    for path in paths:
+        print(path)
 
+while paths:
+    # print(len(paths))
+    # print('paths:')
+    # print_paths(paths)
+    # print()
+    path = paths.pop()
 
-def update_grid(beams):
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if grid[row][col] != 'S' and (row, col) in beams:
-                grid[row][col] = '|'
+    current_row, current_col = path[-1]
+    new_row = current_row + 1
 
+    if new_row > grid_height - 1:
+        path.append((new_row, current_col))
+        solutions.add(str(path))
+        # print(f'{len(solutions)} paths complete ({len(paths)} paths)')
+        continue
 
-iterations = 0
+    if grid[new_row][current_col] == '^':
+        left_path = path.copy()
+        left_path.append((new_row, current_col - 1))
+        paths.append(left_path)
 
-while True:
-    # if iterations > 100:
-    #     break
-    # iterations += 1
+        right_path = path.copy()
+        right_path.append((new_row, current_col + 1))
+        paths.append(right_path)
+        # print(f'path splitting ({len(paths)} paths)')
+    else:
+        new_path = path.copy()
+        new_path.append((new_row, current_col))
+        paths.append(new_path)
+        # print(f'path continuing ({len(paths)} paths)')
 
-    # print('beams at start of loop')
-    # update_grid(beams)
-    # print_grid()
-
-    moved_beams: List[Tuple[int, int]] = []
-    moved_beam = False
-    # Move all beams down one (if possible)
-    for beam in beams:
-        # print(f'looking at beam {beam}')
-        if beam[0] < grid_height - 1:
-            # print(f'moving beam from ({beam[0], beam[1]}) to ({beam[0] + 1}, {beam[1]})')
-            new_beam = (beam[0] + 1, beam[1])
-            moved_beams.append(new_beam)
-            moved_beam = True
-        else:
-            # print(f'not moving beam from ({beam[0], beam[1]})')
-            moved_beams.append(beam)
-
-    if not moved_beam:
-        break
-
-    beams = moved_beams
-
-    # print('beams after moving down')
-    # print(beams)
-
-    # Check for splits
-    new_beams: List[Tuple[int, int]] = []
-    for beam in beams:
-        beam_row, beam_col = beam
-        if grid[beam_row][beam_col] == '^':
-            new_beam_1 = (beam_row, beam_col - 1)
-            new_beam_2 = (beam_row, beam_col + 1)
-            new_beams.append(new_beam_1)
-            new_beams.append(new_beam_2)
-            num_splits += 1
-        else:
-            new_beams.append(beam)
-
-    beams = list(set(new_beams))
-
-    # print('beams after splitting')
-    # print(beams)
-
-    print()
-
-print(num_splits)
-
-# 877 is too low
+print(len(solutions))
