83c01053e3ade97cba918759d9397979cfccd3c9
Brian Eck <brian.eck@patentbots.com>
2025-12-09 22:57:55 -0500

    wip

--- 2025/09/09b.py
+++ 2025/09/09b.py
@@ -3,7 +3,7 @@ from typing import Tuple, List
 
 lines = []
 
-with open('09.input', 'r') as f:
+with open('09.test', 'r') as f:
     lines = f.readlines()
 
 lines = [line.strip() for line in lines]
@@ -18,12 +18,48 @@ for line in lines:
 max_area = 0
 max_tiles: List[Tuple[int, int]] = []
 
+
+def isRedOrGreen(tile: Tuple[int, int]) -> bool:
+    print(f'is tile {tile} red or green?')
+    if tile in red_tiles:
+        print("\tit's red")
+        return True
+
+    red_tile_in_column = False
+    red_tile_in_row = False
+
+    for t in red_tiles:
+        if t[0] == tile[0]:
+            print("\tfound a red tile in its column")
+            red_tile_in_column = True
+            continue
+        if t[1] == tile[1]:
+            print("\tfound a red tile in its row")
+            red_tile_in_row = True
+            continue
+
+    if red_tile_in_column and red_tile_in_row:
+        print("\tit's green")
+    else:
+        print("it's neither red nor green")
+
+    return red_tile_in_column and red_tile_in_row
+
+
 for tile in red_tiles:
     for tile2 in red_tiles:
         if tile == tile2:
             continue
 
         if tile[0] < tile2[0] and tile[1] < tile2[1]:
+            tile3: Tuple[int, int] = (tile2[0], tile[1])
+            tile4: Tuple[int, int] = (tile[0], tile2[1])
+            print(f'found red corners at {tile} and {tile2}, checking the other corners {tile3} and {tile4}')
+            if not isRedOrGreen(tile3) or not isRedOrGreen(tile4):
+                print()
+                continue
+            print()
+
             area = (tile2[0] - tile[0] + 1) * (tile2[1] - tile[1] + 1)
             if area > max_area:
                 max_area = area
